c6eaffab2eb57525e20e980591bf7535085e098b
Jared Clemmensen and Kimberly Huynh <kimberly@>
2018-01-10 08:31:48 -0800

    complete adding basic functionality to form.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
