597a3a5d69c91d36b93a1af03ab99b79104980c6
Guest User <Guest@Epicodus-9D.local>
2018-01-16 09:55:13 -0800

    refactor javascript using array and looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var inputsArray = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    inputsArray.forEach(function(input) {
+      var userInput = $("#" + input).val();
+      $("." + input).text(userInput);
+    });
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
